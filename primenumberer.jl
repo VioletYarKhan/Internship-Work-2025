6304a01f53f23a5f4bc3128c4d2d43f671c80e7b
VioletYarKhan <nicholasyarkhan@gmail.com>
2025-06-25 10:04:47 -0400

    Added julia file to remember how to use the language

---
+++ primenumberer.jl
@@ -0,0 +1,25 @@
+function calcPrimes()
+    currNum = 3
+    println("2")
+    primes = []
+    while true
+        isPrime = true
+        for num in primes
+            if (num > sqrt(currNum))
+                break
+            end
+            if (currNum%num == 0)
+                isPrime = false
+                break
+            end
+        end
+        if isPrime
+            append!(primes, currNum)
+            println(currNum)
+        end
+        currNum += 2
+    end
+end
+
+
+calcPrimes()
